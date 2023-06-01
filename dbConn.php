7f0aa2ef97382fc636cf785474f099d2deac12dd
Pawani Pathirana <92365939+PawaniPathirana@users.noreply.github.com>
2023-06-01 11:16:00 +0530

    db connection

---
+++ dbConn.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$host="localhost";
+$username="root";
+$password="";
+$dbname="fertidb";
+
+$con=mysqli_connect($host,$username,$password,$dbname,3307);
+
+
+if(!$con){
+	
+	die('Could not connect mysql sever:'.mysql_error());
+}
+?>
